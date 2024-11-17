b0d531a65b9e46573dd65e3d571d39b8cd1790a2
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2024-11-17 23:22:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function sendMessage() {
   document.getElementById('user-message').value = '';
 
   try {
-    const response = await fetch('https://backend-chatbot-05v9.onrender.com/get_response', {
+    const response = await fetch('http://127.0.0.1:5000/get_response', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
